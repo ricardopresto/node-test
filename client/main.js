bc00054c92b7e102c47a335d50bae329b2049187
ricardo <ricardo@ricardosmacbook.home>
2020-04-20 14:29:07 +0200

    added build script

--- client/main.js
+++ client/main.js
@@ -12,6 +12,6 @@ var infoArray = [{ one: "spam" }, { two: "eggs" }];
 function sendBtnClick() {
   axios
     .post(url, infoArray)
-    .then(res => console.log(res.data))
+    .then(res => console.log(res.data[0]))
     .catch(err => console.log(err));
 }
--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
